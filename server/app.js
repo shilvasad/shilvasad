1daeedb49b5268c519cf70adb2a539c21945b7fb
Md Asad Chowdhary <shilvasad@gmail.com>
2025-05-20 00:32:15 +0600

    fix: update API routing to use root path

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ import apiRouter from "./routes/index.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/api", apiRouter);
+app.use("/", apiRouter);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
