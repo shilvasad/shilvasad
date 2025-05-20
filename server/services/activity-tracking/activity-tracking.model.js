f8f0d58f6634729345a6ef1d914510e2baadfc66
Md Asad Chowdhary <shilvasad@gmail.com>
2025-05-20 17:51:11 +0600

    fix: now Activity duration will be count starting from startTime to endTime.

--- server/services/activity-tracking/activity-tracking.model.js
+++ server/services/activity-tracking/activity-tracking.model.js
@@ -23,9 +23,25 @@ const activityTrackingSchema = new Schema({
     },
     activityDuration: {
         type: Number,
-        required: true
     },
 })
+
+
+activityTrackingSchema.pre('save', function(next){
+    if(this.activityStartTime && this.activityEndTime){
+        const durationMs = this.activityEndTime.getTime() - this.activityStartTime.getTime()
+        this.activityDuration = Math.round(durationMs / (1000 * 60))
+
+        if(this.activityDuration <0){
+            return next(new  Error('End time must be after start time'))
+        }
+    }
+    else{
+        return next (new Error('Start time and end time are required'))
+    }
+    next()
+})
+
 const ActivityTracking = mongoose.model('Activity', activityTrackingSchema)
 
 export default ActivityTracking
